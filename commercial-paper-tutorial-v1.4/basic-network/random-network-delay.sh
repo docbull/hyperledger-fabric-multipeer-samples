d1a1b69b671efbf3d489491af0edc9c02c1163ab
krh <fudghks21@gmail.com>
2021-01-20 12:15:37 +0900

    peer radom network delay configuration shell scripts

---
+++ commercial-paper-tutorial-v1.4/basic-network/random-network-delay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Dongguk Univ, Gyeon-ju Campus., INLab
+# REF) https://wahabjawed.medium.com/extending-hyperledger-fabric-network-adding-a-new-peer-4f52f70a7217
+
+# shell script for invoking issue transactions
+
+echo
+echo "installing netem"
+SEQUENCE=$(seq 0 20)
+for i in $SEQUENCE
+do
+    rand=$(((RANDOM % 80) + 20))
+    NET_DELAY=${rand}ms
+    docker exec peer${i}.org1.example.com tc qdisc del dev eth0 root
+    docker exec peer${i}.org1.example.com tc qdisc add dev eth0 root netem delay $NET_DELAY
+    echo "peer${i} : $NET_DELAY"
+done
